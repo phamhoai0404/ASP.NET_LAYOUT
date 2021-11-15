b10c64fd53aeb5b4a329dc94545a207f610e9dc9
Pham Hoai <hoai04042000@gmail.com>
2021-11-15 14:45:39 +0700

    Layout Admin không ổn lắm

---
+++ LayoutNhom8/LayoutNhom8/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutNhom8.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QuanlyDanhmucSanpham()
+        {
+            return View();
+        }
+        public ActionResult QuanlyDonhang()
+        {
+            return View();
+        }
+        public ActionResult QuanlySanpham()
+        {
+            return View();
+        }
+        public ActionResult QuanlyTaikhoan()
+        {
+            return View();
+        }
+        public ActionResult ThongtinDangnhap()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+    }
+}
